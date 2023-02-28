5da398eea7e17acce1502c0692baebe8ac7c57fa
Karrius <Ecetris@gmail.com>
2023-02-28 21:22:21 +0100

    + memoPro project, - Pizzeria website

--- src/components/Projects/projectsData.ts
+++ src/components/Projects/projectsData.ts
@@ -24,8 +24,37 @@ import WF_idk from '../../assets/WF-idk.webp'
 import WF_idk_Blur from '../../assets/WF-idk-blur.webp'
 import WF_week from '../../assets/WF-week.webp'
 import WF_week_Blur from '../../assets/WF-week-blur.webp'
+import notes_1 from '../../assets/notes-1.webp'
+import notes_1_Blur from '../../assets/notes-1-blur.webp'
+import notes_2 from '../../assets/notes-2.webp'
+import notes_2_Blur from '../../assets/notes-2-blur.webp'
+import notes_3 from '../../assets/notes-3.webp'
+import notes_3_Blur from '../../assets/notes-3-blur.webp'
 
 export const projectsData = [
+  {
+    slidesSrcArray: [
+      { src: notes_3, srcBlur: notes_3_Blur, alt: 'MemoPro app screenshot' },
+      { src: notes_2, srcBlur: notes_2_Blur, alt: 'MemoPro app screenshot' },
+      { src: notes_1, srcBlur: notes_1_Blur, alt: 'MemoPro app screenshot' },
+    ],
+    githubLink: 'https://github.com/m-jezewski/memo-pro',
+    demoLink: 'https://memo-pro.vercel.app/',
+    title: 'MemoPro',
+    description: 'MemoPro is a note-taking application built with Next.js, Prisma ORM and Docker. With MemoPro, users can easily create, edit, and delete notes, as well as give them titles and sort them in any order they desire. MemoPro also provides a range of functionalities, including user authentication via NextAuth.js and data-fetching with Tanstack React Query, making it a reliable and secure option for note-taking on the go.',
+    techStack: [
+      { name: 'Next.js', backgroundColor: 'white', color:'black' },
+      { name: 'Prisma', backgroundColor: 'rgb(90, 103, 216)', color: 'white' },
+      { name: 'Docker', backgroundColor: '#003f8c', color: 'white'},
+      { name: 'typescript', backgroundColor: '#3178c6', color: 'white' },
+      { name: 'react', backgroundColor: '#282c34', color: '#61dafb' },
+      { name: 'NextAuth', backgroundColor: '#9333ea', color: '#99f6e4' },
+      { name: 'Tanstack React Query', backgroundColor: '#f59e0b', color: 'black'},
+      { name: 'Tailwind Css', backgroundColor: '#C7E4EA', color: 'black' },
+      { name: 'Formik', backgroundColor: '#f5e3fd', color: 'black' },
+      { name: 'Dnd-kit', backgroundColor: 'black', color: 'white' },
+    ]
+  },
   {
     slidesSrcArray: [
       { src: TM_Daycal, srcBlur: TM_Daycal_Blur, alt: 'Task manager app screenshot with day view calendar' },
@@ -37,7 +66,7 @@ export const projectsData = [
     demoLink: 'https://mjezewski-taskmanager.netlify.app/',
     title: 'Task Manager',
     description:
-      "Task manager is a complex work management tool and so far my largest and most extensive project. The application uses some popular task tracking solutions (to-do list, semi-kanban board, calendar) and allows user to group and display tasks according to user's preferences so that everyone can customize the application depending on their needs. Additionally, app includes a feature to define larger goals and track their progress. I created it with typescript, react, headless ui library (to ensure proper accessibility and keyboard movement on components like dialogs and popups) and used firebase as a backend (auth & database). Feel free to check live demo and readme on github for more info.",
+      "Task Manager is a comprehensive work management tool, and my largest and most extensive project to date. The application offers multiple task tracking solutions including to-do list, kanban board, and calendar. Users can group and display tasks according to their preferences and customize the application to suit their needs. Additionally, the app includes a feature to define larger goals and track their progress. Task Manager is built with TypeScript, React, Headless UI library (for accessibility and keyboard movement on components like dialogs and popups), and Firebase (for authentication and database). Check out the live demo and readme on GitHub for more information.",
     techStack: [
       { name: 'typescript', backgroundColor: '#3178c6', color: 'white' },
       { name: 'react', backgroundColor: '#282c34', color: '#61dafb' },
@@ -58,7 +87,7 @@ export const projectsData = [
     githubLink: 'https://github.com/m-jezewski/weather-forecast-app',
     demoLink: 'https://mjezewski-weatherapp.netlify.app/',
     description:
-      'Application for checking weather forecast for the next few days in most locations in Poland. The aim of the project was to learn typescript, Tailwind Css and use of fetch api with react. Free API used in this project: https://openweathermap.org/api',
+      "Weather Forecast is an application that allows users to check the weather forecast for the next few days in most locations in Poland. The project's aim was to learn TypeScript, Tailwind CSS, and the use of fetch API with React. Free API used in this project: https://openweathermap.org/api",
     techStack: [
       { name: 'typescript', backgroundColor: '#3178c6', color: 'white' },
       { name: 'react', backgroundColor: '#282c34', color: '#61dafb' },
@@ -67,24 +96,24 @@ export const projectsData = [
       { name: 'fetch api', backgroundColor: 'black', color: 'white' },
     ],
   },
-  {
-    slidesSrcArray: [
-      { src: PW_home, srcBlur: PW_home_Blur, alt: 'Screenshot of pizzeria site home page' },
-      { src: PW_Menu, srcBlur: PW_Menu_Blur, alt: 'Screenshort of pizzeria site menu page' },
-      { src: PW_home2, srcBlur: PW_home2_Blur, alt: 'Screenshot of pizzera site about section' },
-    ],
-    title: 'Pizzeria Site',
-    githubLink: 'https://github.com/m-jezewski/pizzeria-site-react-mui',
-    demoLink: 'https://mjezewski-pizzeria.netlify.app',
-    description:
-      'Simple pizzeria website. The aim of the project was to create a relatively aesthetic site while learning React, React Router and components from Material Ui v5.',
-    techStack: [
-      { name: 'Javascript', color: 'black', backgroundColor: '#FFFE27' },
-      { name: 'react', backgroundColor: '#282c34', color: '#61dafb' },
-      { name: 'react router', backgroundColor: 'white', color: 'black' },
-      { name: 'Material Ui v5', color: 'white', backgroundColor: 'royalblue' },
-    ],
-  },
+  // {
+  //   slidesSrcArray: [
+  //     { src: PW_home, srcBlur: PW_home_Blur, alt: 'Screenshot of pizzeria site home page' },
+  //     { src: PW_Menu, srcBlur: PW_Menu_Blur, alt: 'Screenshort of pizzeria site menu page' },
+  //     { src: PW_home2, srcBlur: PW_home2_Blur, alt: 'Screenshot of pizzera site about section' },
+  //   ],
+  //   title: 'Pizzeria Site',
+  //   githubLink: 'https://github.com/m-jezewski/pizzeria-site-react-mui',
+  //   demoLink: 'https://mjezewski-pizzeria.netlify.app',
+  //   description:
+  //     "Pizzeria Site is a simple website designed for a fictional pizzeria. The project's aim was to create an aesthetically pleasing site while learning React, React Router, and components from Material UI v5",
+  //   techStack: [
+  //     { name: 'Javascript', color: 'black', backgroundColor: '#FFFE27' },
+  //     { name: 'react', backgroundColor: '#282c34', color: '#61dafb' },
+  //     { name: 'react router', backgroundColor: 'white', color: 'black' },
+  //     { name: 'Material Ui v5', color: 'white', backgroundColor: 'royalblue' },
+  //   ],
+  // },
   {
     slidesSrcArray: [
       { src: tetris_1, srcBlur: tetris_1_Blur, alt: 'Tetris game image' },
@@ -95,7 +124,7 @@ export const projectsData = [
     demoLink: 'https://mjezewski-tetris.netlify.app',
     title: 'Tetris Game',
     description:
-      'Tetris is one of my most interesting applications created using plain javascript. The project was created in my early days of learning js so the code itself is very unreadable, but today I would definitely be able to do it in a much better, cleaner and smarter way. Nevertheless, I decided to share it because I think that the game itself is very interesting, unique and creating it was a lot of fun! I also made for it a really simple express.js backend which connects to the mongoDB database to create a player scoreboard',
+      "Tetris Game is a interesting game that I created using plain JavaScript. Although the code itself is not the highest quality, I decided to include it in my portfolio because I believe the game is fascinating and creating it was a lot of fun! The project also includes a simple Express.js backend that connects to a MongoDB database to create a player scoreboard.",
     techStack: [
       { name: 'Javascript', color: 'black', backgroundColor: '#FFFE27' },
       { name: 'CSS', color: 'white', backgroundColor: 'royalblue' },
